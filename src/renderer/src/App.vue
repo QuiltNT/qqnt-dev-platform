8d88829e339f82cfdc82d342986d1d14758869e3
LateDream <127290936+LateDreamXD@users.noreply.github.com>
2025-08-06 08:31:21 +0800

    Initial commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+import Versions from './components/Versions.vue';
+</script>
+
+<template>
+	<HelloWorld />
+	<Versions />
+</template>
